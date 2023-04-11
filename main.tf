5176a728a7d3133ab116ae7bae6315b7a7f384aa
Joao Silva <jpaulossilva@hotmail.com.br>
2023-04-10 22:42:37 -0300

    update terraform

--- main.tf
+++ main.tf
@@ -21,17 +21,15 @@ resource "aws_instance" "app_server" {
   ami           = "ami-007855ac798b5175e"
   instance_type = "t2.micro"
   key_name = "ec2"
-  # Configuração do user_data (Instala coisas dentro da ec2)
   user_data = <<-EOF
-                 #!/bin/bash
-                 cd /home/ubuntu
-                 echo "<h1> Feito com terraform, Aprende Joao </h1>" >index.html
-                 nohup busybox httpd -f -p 8080 &     
-
-                 EOF  
+                #!/bin/bash
+                cd /home/ubuntu
+                echo “<h1>Mensagem a ser mostrada</h1>” > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
 # Nome da Instancia
   tags = {
-    Name = "Nova EC2"  
+    Name = "EC2 With USER_DATA"  
   }
 }
